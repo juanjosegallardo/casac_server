da087ff1dce84ffbb23cd47f9bf8245c848bc1fb
isc.juan.gallardo@gmail.com <isc.juan.gallardo@gmail.com>
2022-08-19 18:10:06 -0500

    first commit

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Producto;
+
+
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Producto::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $producto =  new Producto();
+        $producto->codigo = $request->codigo;
+        $producto->descripcion = $request->descripcion;
+        $producto->precio_compra =  $request->precio_compra;
+        $producto->precio_venta = $request->precio_venta;
+        $producto->venta_granel = $request->venta_granel;
+        $producto->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Producto::findOrFail($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+       
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $producto=Producto::findOrFail($id);
+        $producto->codigo = $request->codigo;
+        $producto->descripcion = $request->descripcion;
+        $producto->precio_compra =  $request->precio_compra;
+        $producto->precio_venta = $request->precio_venta;
+        $producto->venta_granel = $request->venta_granel;
+        $producto->update();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $producto=Producto::findOrFail($id);
+        $producto->delete();
+    }
+}
